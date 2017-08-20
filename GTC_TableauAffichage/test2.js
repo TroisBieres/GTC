4a7b153d5224091a5b7102b96edc64055d471257
antony <antony.turpin@gmail.com>
2017-08-21 01:30:09 +0200

    Version de test des API fonctionnel

---
+++ GTC_TableauAffichage/test2.js
@@ -0,0 +1,16 @@
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest
+var xhr = new XMLHttpRequest()
+xhr.open('GET', 'http://localhost:3000/api/T_Associations', true)
+xhr.onload = function (e) {
+  if (xhr.readyState === 4) {
+    if (xhr.status === 200) {
+      console.log(xhr.responseText)
+    } else {
+      console.error(xhr.statusText)
+    }
+  }
+}
+xhr.onerror = function (e) {
+  console.error(xhr.statusText)
+}
+xhr.send(null)
